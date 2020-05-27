d0d5ddd5bebfd47651411543405b60c8679dfa4c
KROWNED <thestuzzy@gmail.com>
2020-05-26 22:55:55 -0700

    Initial Commit

---
+++ APPLICATION/JS/index.js
@@ -0,0 +1,36 @@
+const {remote, ipcRenderer} = require('electron');
+const $ = require('jquery');
+
+document.getElementById('min').addEventListener('click', minWindow);
+document.getElementById('max').addEventListener('click', maxWindow);
+document.getElementById('exit').addEventListener('click', exitWindow);
+
+function minWindow () {
+    var window = remote.BrowserWindow.getFocusedWindow();
+    window.minimize();
+}
+
+function maxWindow () {
+    var window = remote.BrowserWindow.getFocusedWindow();
+    if(window.isMaximized()){
+        window.unmaximize();
+        $("#maxmin").removeClass('fa-compress').addClass('fa-expand');
+    }else{
+        window.maximize();
+        $("#maxmin").removeClass('fa-expand').addClass('fa-compress');
+    }
+}
+
+function exitWindow () {
+    var window = remote.BrowserWindow.getFocusedWindow();
+    window.close();
+}
+
+function fullscreen(){
+    var window = remote.getCurrentWindow();
+    window.focus();
+    window.setResizable(true);
+    window.setSize(1100, 750);
+    window.setMinimumSize(1100, 750);
+    window.maximize();
+}
---
+++ Client.js
@@ -0,0 +1,76 @@
+const { app, BrowserWindow, ipcMain } = require('electron')
+let win;
+
+const { autoUpdater } = require('electron-updater');
+
+function createWindow () {
+  win = new BrowserWindow({
+    width: 350,
+    height: 500,
+    frame: false,
+    icon: "APPLICATION/IMAGES/icon.ico",
+    webPreferences: {
+      nodeIntegration: true
+    }
+  })
+
+//DO NOT TOUCH THIS LINE RIGHT HERE 
+  win.loadFile('APPLICATION/HTML/load.html')
+//THIS ONE DONT TOUCH IT ^^^^^^^^^
+
+  win.on('closed', () => {
+    win = null
+  })
+
+    autoUpdater.checkForUpdatesAndNotify();
+}
+
+
+
+app.on('ready', createWindow)
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  if (win === null) {
+    createWindow()
+  }
+})
+
+autoUpdater.on('checking-for-update', () => {
+  win.webContents.send('checking_for_update');
+});
+
+autoUpdater.on('update-available', () => {
+  win.webContents.send('update_available');
+});
+
+autoUpdater.on('update-not-available', () => {
+  win.webContents.send('update_not_available');
+});
+
+autoUpdater.on('update-downloaded', () => {
+  win.webContents.send('update_downloaded');
+});
+
+
+
+autoUpdater.on('download-progress', (progressObj) => {
+  let log_message = ' (' + progressObj.transferred + "/" + progressObj.total + ')';
+  log_message = log_message + ' - Downloaded ' + progressObj.percent + '%';
+  win.webContents.send('download_progress', log_message);
+})
+
+
+
+ipcMain.on('app_version', (event) => {
+  event.sender.send('app_version', { version: app.getVersion() });
+});
+
+ipcMain.on('restart_app', () => {
+  autoUpdater.quitAndInstall();
+});
